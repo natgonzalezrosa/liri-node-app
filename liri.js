fa6d52a9a6da237610f5dcc8032dbe267d8c83bf
natgonzalezrosa <natgonzalezrosa@gmail.com>
2018-07-21 21:47:20 -0400

    fixed bugs in doWhatItSays function

--- liri.js
+++ liri.js
@@ -25,7 +25,7 @@ switch (command) {
         showMovieInfo();
         break;
     case "do-what-it-says":
-        callLiri();
+        doWhatItSays();
         break;
     default:
         console.log("Error: please type in command again.");
@@ -107,7 +107,9 @@ function showMovieInfo() {
     });
 };
 
-/* function callLiri() {
+function doWhatItSays() {
+
+    var spotify = new Spotify(llaves.spotifyKeys);
 
     fs.readFile("random.txt", "utf8", function (error, data) {
 
@@ -119,23 +121,55 @@ function showMovieInfo() {
         // Then split it by commas (to make it more readable)
         var dataArr = data.split(",");
 
+        console.log(dataArr);
+
         // If first item in array is "my-tweets" then second item in array is run through the getTweets function
         if (dataArr[0] === "my-tweets") {
-            var myTweet = dataArr[1].slice(1, -1)
-            getTweets(myTweet);
+            getTweets(dataArr[1].slice(1, -1));
         }
         
-        // If first item in array is "spotify-this-song" then second item in array is run through the showSongInfo function
-        else if (dataArr[0] === "spotify-this-song") {
-            var mySong = dataArr[1].slice(1, -1);
-            showSongInfo(mySong);
-        }
+        // If first item in array is "spotify-this-song" then second item in array is passed through Spotify's search method to obtain track info
+        if (dataArr[0] === "spotify-this-song") {
+
+            spotify.search({ type: 'track', query: dataArr[1].slice(1, -1)}, function (err, data) {
+
+                // If there is an error then console.log the error
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+
+                // If there is no error then display song information
+                console.log("Artist: " + data.tracks.items[0].artists[0].name +
+                    "\nSong's Name: " + data.tracks.items[0].name +
+                    "\nPreview Link of the Song: " + data.tracks.items[0].preview_url +
+                    "\nAlbum the Song is From: " + data.tracks.items[0].album.name);
+                
+            });
+        };
 
         // If first item in array is "movie-this" then second item in array is run through the showMovieInfo function
-        else if (dataArr[0] === "movie-this") {
-            var myMovie = dataArr[1].slice(1, -1)
-            showMovieInfo(myMovie);
+        if (dataArr[0] === "movie-this") {
+
+            // Run a request to the OMDB API with the movie specified
+            request("http://www.omdbapi.com/?t=" + dataArr[1].slice(1, -1) + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+
+                // If the request is successful (i.e. if the response status code is 200)
+                if (!error && response.statusCode === 200) {
+
+                // Parse the body of the site and recover the title, year, IMBD rating, Rotten Tomatoes rating, Country, language, plot and actors from the movie
+                console.log("Title of the Movie: " + JSON.parse(body).Title +
+                            "\nYear: " + JSON.parse(body).Year +
+                            "\nIMBD Rating: " + JSON.parse(body).imdbRating +
+                            "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1] +
+                            "\nCountry: " + JSON.parse(body).Country +
+                            "\nLanguage: " + JSON.parse(body).Language +
+                            "\nPlot: " + JSON.parse(body).Plot +
+                            "\nActors: " + JSON.parse(body).Actors +
+                            "\n---------------------------------------------------------\n");
+
+                };
+            });
         }
 
     });
-}; */
+}
